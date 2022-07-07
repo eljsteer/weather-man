0df96a41e1b642f4e474f2a718103d4f970a637c
Jason <el.jsteer@gmail.com>
2022-07-07 18:24:07 +1000

    updated weather PAAI fetch calls and started with fucntion to display and append todays weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,43 +46,64 @@ let dt = dayjs().unix();
 let apiKey = "c21a2246b95017f0e7609338479f2597";
 let userLocation = $(".userLoc")
 let cityArray = [];
-let forecastTime = {
-
-}
 
 searchBttn.on("click", function() {
 let cityInput = $("#city-input").val();
-let geoLocAPIURL = "http://api.openweathermap.org/geo/1.0/direct?q="+cityInput+"&limit=20&appid="+apiKey;
+// let geoLocAPIURL = "http://api.openweathermap.org/geo/1.0/direct?q="+cityInput+"&limit=20&appid="+apiKey;
 let weatherAPIURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityInput+"&units=metric&appid="+apiKey;
-// let day5APIURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+apiKey;
 
-userLocation.toggleClass("hide");
+// userLocation.toggleClass("hide");
 
 fetch (weatherAPIURL) 
   .then(function (response) {
   return response.json();
   })
-  .then(function (data) {
-  console.log(data);
-  
-  fetch ("https://api.openweathermap.org/data/2.5/forecast?units=metric&lat="+data.coord.lat+"&lon="+data.coord.lon+"&appid="+apiKey) 
+    .then(function (data) {
+    console.log(data);
+      
+
+    function displayToday() {
+      let todayWeatherIcon = data.weather[0].icon
+      let todayWeather = data.weather[0].main
+      let todayTemp = data.main.temp
+      let todayMaxTemp = data.main.temp_max
+      let todayMinTemp = data.main.temp_min
+      let todayWindSpeed = data.wind.speed
+      let todayWindDeg = data.wind.deg
+      
+
+      $("#city-weather-today").append(cityInput)
+      $("#today-weather-icon").append(todayWeatherIcon)
+
+
+    }
+
+
+
+
+
+// <======== Code to fetch and display 5day Weather Forecast ========>
+    let lats = data.coord.lat;
+    let longs = data.coord.lon;
+
+    let day5APIURL = "https://api.openweathermap.org/data/2.5/forecast?units=metric&lat="+lats+"&lon="+longs+"&appid="+apiKey;
+
+  fetch (day5APIURL) 
   .then(function (response) {
   return response.json();
   })
-  .then(function (data) {
-  console.log(data);
-
-  for (i=0; i<data.list.length; i+=8) {
-    
-  };
+    .then(function (data) {
+    console.log(data);
+      displayToday();
+  // for (i=0; i<data.list.length; i+=8) {
+  
+  // };
 
   });
   });
 
 });
-  // let longitude = data.
-  // let latitude = data.
-  
 
-  
+
+
 
