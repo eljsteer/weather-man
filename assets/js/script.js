589d39e688b17decc50d70621b659b48c3ff73cc
Jason <el.jsteer@gmail.com>
2022-07-09 12:53:32 +1000

    updated API calls and event handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,40 +18,34 @@
     // create variables for the card to display each attribute
     // append to Container
 
+
 const searchBttn = $("#search-bttn");
 const myLocation = $("#my-location")
-let cityInput = $("#city-input").val();
+let cityInputEl = $("#city-input");
 let dt = dayjs().unix();
 let apiKey = "c21a2246b95017f0e7609338479f2597";
 let userLocation = $(".userLoc")
 let cityArray = [];
-let day5APIURL;
 
-
-searchBttn.on("click", function() {
-  // let geoLocAPIURL = "http://api.openweathermap.org/geo/1.0/direct?q="+cityInput+"&limit=20&appid="+apiKey;
-  // userLocation.toggleClass("hide");
-  
-  let cityCoordAPIURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityInput+"&units=metric&appid="+apiKey;
+function cityCoordinates() {
+  selectedCity = cityInputEl.val().trim();
+  let cityCoordAPIURL = "https://api.openweathermap.org/data/2.5/weather?q="+selectedCity+"&units=metric&appid="+apiKey;
   
     fetch (cityCoordAPIURL) 
     .then(function (response) {
-      if(response.status = 200) {
         return response.json();
-  
-      } else if(response.status != 200) {
-          response.innerHTML();
-        };
     })
     .then(function(data) {
-      // <======== Code to fetch and display 5day Weather Forecast ========>
+      console.log(data)
       let lats = data.coord.lat;
       let longs = data.coord.lon;
-        });
-    
-  let day5APIURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+lats+"&lon="+longs+"&exclude=minutely,hourly&appid="+apiKey;
+    });
+}
 
-  function forecast5DayAPI () {
+  // <======== Code to fetch and display 5day Weather Forecast ========>
+  function forecast5DayAPI (lats,longs) {
+    let day5APIURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+lats+"&lon="+longs+"&exclude=minutely,hourly&appid="+apiKey;
+    
     fetch (day5APIURL) 
       .then(function (response) {
         if(response.status = 200) {
@@ -63,8 +57,9 @@ searchBttn.on("click", function() {
       })
       .then(function (data) {
         console.log(data);
-        displayToday();
       });
     };
-  forecast5DayAPI();
-});
+
+
+
+searchBttn.on("click", cityCoordinates);
