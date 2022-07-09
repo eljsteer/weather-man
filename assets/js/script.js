debe7190ff4f2514a7b2798b08ad3b9babc2daea
Jason <el.jsteer@gmail.com>
2022-07-09 19:45:36 +1000

    displayed current day data on Screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,7 @@
 const searchBttn = $("#search-bttn");
 
 let cityInputEl = $("#city-input");
-let timeNow = dayjs().format("dddd DD, MMM, YYYY hh:mm A")
+let timeNow = dayjs().format("DD, MMM, YYYY hh:mm A")
 let apiKey = "c21a2246b95017f0e7609338479f2597";
 // let userLocation = $(".userLoc")
 // const myLocation = $("#my-location")
@@ -74,37 +74,61 @@ function weatherData(currentCityInfo) {
 };
 
 function displayWeather (currentCityInfo, dataCurrent, dataDaily) {
-  
-  let currentTime = dataCurrent.dt;
+  let iconcode = dataCurrent.weather[0].icon;
   let currentTemp = dataCurrent.temp;
   let currentWindSpd = dataCurrent.wind_speed;
   let currentWindDirection = dataCurrent.wind_deg;
   let currentHumidity = dataCurrent.humidity;
   let currentUVI = dataCurrent.uvi;
+  let currentWeatherBody = $("#weather-today-body");
   
   // let currentWeatherArray = [];
   // currentWeatherArray.push[currentTemp,currentWindSpd,currentWindDirection,currentHumidity,currentUVI]
 
   // Code to display Selected Cities Current Weather Today
-
   // Bootstrap container Styling for Weather Today 
-  let currentWeatherEl = $("#weather-today")
-  currentWeatherEl.addClass("card col-9 mx-3");
+  let currentWeatherEl = $("#weather-today-header")
+  currentWeatherEl.addClass("card col-9 mx-3 row");
 
   // Code to display city Name and Time
-  let currentWeatherBody = $("<div>");
+  let weatherTodayEl = $("<div>");
   let currentCityNameEl = $("<h4>");
   let currentTimeEl = $("<h5>");
   currentTimeEl.text(timeNow);
   currentCityNameEl.text(currentCityInfo.cityName);
 
-  currentWeatherBody.append(currentCityNameEl);
-  currentWeatherBody.append(currentTimeEl);
-  currentWeatherEl.append(currentWeatherBody);
+  weatherTodayEl.append(currentCityNameEl);
+  weatherTodayEl.append(currentTimeEl);
+  currentWeatherEl.append(weatherTodayEl);
   
-  $("#current-time").text(timeNow);
-  
-  // // Code to append currentTemp
-  // let currentWeatherData = $("li");
-  // currentWeatherData.text("")
+    // Code to append Weather Icon
+    let currentWeatherIcon = $("<img>");
+    currentWeatherIcon.attr("src", "https://openweathermap.org/img/w/" + iconcode + ".png")
+    currentWeatherEl.append(currentWeatherIcon);
+
+
+  // Code to Display currentTemp
+  let currentTempEl = $("<div>");
+  currentTempEl.text(`Temperature: ${currentTemp}℃`)
+  currentWeatherBody.append(currentTempEl);
+
+  // Code to Display current WindSpeed
+  let currentWindSpeedEl = $("<div>");
+  currentWindSpeedEl.text(`Wind Speed : ${currentWindSpd}km/h`)
+  currentWeatherBody.append(currentWindSpeedEl);
+
+  // Code to Display Wind direction
+  let currentWindDirEl = $("<div>");
+  currentWindDirEl.text(`Wind Direction: ${currentWindDirection}`)
+  currentWeatherBody.append(currentWindDirEl);
+
+  // Code to Display Humidity
+  let currentHumidityEl = $("<div>");
+  currentHumidityEl.text(`Humidity: ${currentHumidity}%`)
+  currentWeatherBody.append(currentHumidityEl);
+
+  // Code to Display UV Index
+  let currentUVIndexEL = $("<div>");
+  currentUVIndexEL.text(`Current Temp: ${currentTemp}`)
+  currentWeatherBody.append(currentTempEl);
 };
